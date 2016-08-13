cd47aa582ad807bfdd7221148c4d63a74fe59417
Gerald <gera2ld@163.com>
2016-08-13 11:47:37 +0800

    remove unuseful files

--- tests/socks4client.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# coding=utf-8
-from base import test
-from socks.client import SOCKS4Client
-
-test(SOCKS4Client)
--- tests/socks5client.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# coding=utf-8
-from base import test
-from socks.client import SOCKS5Client
-
-test(SOCKS5Client)
