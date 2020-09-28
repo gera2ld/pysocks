ee9121aa8530062e8c10e56b034c9844e53cecf7
Gerald <gera2ld@163.com>
2020-09-28 19:49:52 +0800

    feat: add timeout for forwarding

--- gera2ld/socks/utils/base.py
+++ gera2ld/socks/utils/base.py
@@ -3,9 +3,12 @@ import asyncio
 EMPTY_ADDR = '0.0.0.0', 0
 BUF_SIZE = 4096
 
-async def forward_data(reader, writer=None, count=None):
+async def forward_data(reader, writer=None, count=None, timeout=None):
     while True:
-        data = await reader.read(BUF_SIZE)
+        coro = reader.read(BUF_SIZE)
+        if timeout is not None:
+            coro = asyncio.wait_for(coro, timeout)
+        data = await coro
         if not data: break
         if count is not None:
             count(len(data))
@@ -20,11 +23,11 @@ class Counter:
     def count(self, size):
         self.value += size
 
-async def forward_pipes(reader, writer, remote_reader, remote_writer):
+async def forward_pipes(reader, writer, remote_reader, remote_writer, local_timeout=None, remote_timeout=None):
     local_counter = Counter()
     remote_counter = Counter()
-    task_local = asyncio.create_task(forward_data(reader, remote_writer, local_counter.count))
-    task_remote = asyncio.create_task(forward_data(remote_reader, writer, remote_counter.count))
+    task_local = asyncio.create_task(forward_data(reader, remote_writer, local_counter.count, timeout=local_timeout))
+    task_remote = asyncio.create_task(forward_data(remote_reader, writer, remote_counter.count, timeout=remote_timeout))
     done, pending = await asyncio.wait(
         [task_local, task_remote],
         return_when=asyncio.FIRST_COMPLETED,
